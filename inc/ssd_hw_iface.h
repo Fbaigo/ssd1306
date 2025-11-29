a8087839fcb96e10344e5b26ebd8adc069b300f9
Federico Baigorria <febaigo@yahoo.com.ar>
2025-11-29 10:11:19 -0300

    Added hardware interface source file for target microcontrollers

--- inc/ssd_hw_iface.h
+++ inc/ssd_hw_iface.h
@@ -1,42 +1,41 @@
 /*!
- *  @file 	   ssdHardware.h
+ *  @file 	   ssd_hw_iface.h
  *  @brief     SSD1306 128x64 LCD library.
- *  @details   Hardware dependencies needed for I2C communication. This header file can be used
- *  		   in application defined hardware initialization for I2C.
+ *  @details   Hardware dependencies needed for I2C communication. This header file is used
+ *  		   by the ssd1306 device initialization and communication
  *
- *  @warning   Needs to be edited by the user accordingly.
+ *  @warning   Application dependent. User must edit this as needed
  *  @author    Federico Baigorria
- *  @date      03-24-2020
+ *  @date      11-29-2025
  *  @copyright GNU Public License.
  */
 
-#ifndef __SSD1306_INC_SSDHARDWARE_H__
-#define __SSD1306_INC_SSDHARDWARE_H__
+#ifndef __SSD_HW_IFACE_H__
+#define __SSD_HW_IFACE_H__
 
-/* Include here all the board and hardware header files needed for SPI peripheral
- * control and communication
- */
+#define USING_ESP8266X	1
+#define USING_LPC1769	0
 
-///! For LPC1769 
-//#include "board.h"
-//#include "i2c_17xx_40xx.h"
+#if USING_LPC1769
 
-///! For ESP8266x
+#include "board.h"
+#include "i2c_17xx_40xx.h"
+
+#endif
+
+#if USING_ESP8266X
+
 #include "driver/i2c.h"
+#include "gpio.h"
 #include "FreeRTOS.h"
 
-///! Hardware macro definitions
-//#define SSD_I2C		I2C0			///! I2C ID Port to be used
+#define I2C_SCL_IO      GPIO_NUM_5  ///! gpio number for I2C master clock
+#define I2C_SDA_IO      GPIO_NUM_4  ///! gpio number for I2C master data
 
-#define I2C_EXAMPLE_MASTER_SCL_IO           5                /*!< gpio number for I2C master clock */
-#define I2C_EXAMPLE_MASTER_SDA_IO           4               /*!< gpio number for I2C master data  */
-#define I2C_EXAMPLE_MASTER_NUM              I2C_NUM_0        /*!< I2C port number for master dev */
-#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE   0                /*!< I2C master do not need buffer */
-#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE   0                /*!< I2C master do not need buffer */
+#define ACK_EN          0x1         ///! I2C master will check ack from slave*/
+#define ACK_DIS         0x0         ///! I2C master will not check ack from slave */
 
-#define ACK_CHECK_EN                        0x1              /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS                       0x0              /*!< I2C master will not check ack from slave */
-#define WRITE_BIT                           I2C_MASTER_WRITE /*!< I2C master write */
-#define READ_BIT                            I2C_MASTER_READ  /*!< I2C master read */
 
-#endif /* __SSD1306_INC_SSDHARDWARE_H__ */
+#endif
+
+#endif /* __SSD_HW_IFACE_H__ */
--- src/ssd_hw_iface.c
+++ src/ssd_hw_iface.c
@@ -1,22 +1,42 @@
 #include "ssd_hw_iface.h"
 
-#define USING_ESP8266	1
-#define USING_LPC1769	0
-
 /**
-@fn static void sddOledWrite(uint8_t *ssdData, uint8_t ssdNbytes)
-@detail Refer to SSD1306 reference manual serial interface communication chapter for details.
-		As default display's slave address is 0x3C and it's defined in S1306_SLAVE_ADDR.
-		Standard I2C protocol communication.
-
-@warning This function is hardware dependent and should be configured accordingly.
-@param ssdData command/data stream to be sent.
-@param ssdNbytes number of bytes to be sent.
-@return void
+ * @file		ssd_hw_iface.c
+ * @brief		I2C hardware interface for different target microcontrollers or SoC
+ * @details   
+ * The SSD1306 HAL requires two functions, an I2C write method and an I2C
+ * hardware initialization.
+ * 
+ * Refer to SSD1306 reference manual serial interface communication chapter for details
+ * Default display's slave address is 0x3C (S1306_SLAVE_ADDR in ssd1306.h header file)
+ * Requires a standard I2C protocol communication
+ *
+ * Use the following write function prototype 
+ * void (*i2c_write)(uint32_t const slave_addr, uint8_t *data, uint32_t nbytes);
+ * 
+ * Use the following hardware init function prototype
+ * void (*i2c_hw_init)(void);
+ * @author    Federico Baigorria
+ * @version   0.2.0
+ * @date      11-29-2025
+ * @copyright GNU Public License.
 */
 
-#if
-static void lp1769_i2c_write(uint32_t const slave_addr, uint8_t *data, uint32_t nbytes){
+#if USING_LPC1769
+
+/**
+* @fn void lp1769_i2c_write(uint32_t const slave_addr, uint8_t *data, uint32_t nbytes)
+* @brief I2C hardware interface for LPC1769
+* @details 
+* I2C firmware implementation for the LPC1769 using the default I2C port
+* @warning This source file is hardware dependent and must be edited for the target microcontroller, SoC, etc. 
+* @param slave_addr slave address
+* @param data command/data stream to be sent
+* @param nbytes number of bytes to be sent
+* @return void
+*/
+
+void lp1769_i2c_write(uint32_t const slave_addr, uint8_t *data, uint32_t nbytes){
 	I2C_XFER_T sddBuffer;
 
 	///! Preparing datagram for continuos command transmision
@@ -31,12 +51,44 @@ static void lp1769_i2c_write(uint32_t const slave_addr, uint8_t *data, uint32_t 
 	Chip_I2C_MasterTransfer(SSD_I2C, &sddBuffer);
 }
 
-static void esp8266_i2c_write(uint32_t const slave_addr, uint8_t *data, uint32_t nbytes){
+/**
+* @fn void esp8266_i2c_init(void)
+* @brief I2C hardware interface for ESP8266X
+* @details 
+* I2C hardware initialization. 
+* Internal pull-up resistors are not required since the SSD1306 board has soldered pull-up resistors (always check the board)
+* @warning This function is hardware dependent and must be created for the target microcontroller, SoC, etc. 
+* @return void
+*/
+
+void lpc1769_i2c_init(void){
+	Board_I2C_Init(I2C0);
+	Chip_I2C_SetClockRate(I2C0, 1000000);
+	Chip_I2C_SetMasterEventHandler(I2C0, Chip_I2C_EventHandlerPolling);
+}
+
+#endif
+
+#if USING_ESP8266X
+
+/**
+* @fn void esp8266_i2c_write(uint32_t const slave_addr, uint8_t *data, uint32_t nbytes)
+* @brief I2C hardware interface for ESP8266X
+* @details 
+* I2C firmware implementation for the ESP8266X using the default I2C0 port
+* @warning This function is hardware dependent and must be created for the target microcontroller, SoC, etc. 
+* @param slave_addr slave address
+* @param data command/data stream to be sent
+* @param nbytes number of bytes to be sent
+* @return void
+*/
+
+void esp8266_i2c_write(uint32_t const slave_addr, uint8_t *data, uint32_t nbytes){
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
     ESP_ERROR_CHECK(i2c_master_start(cmd));
-	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, slave_addr << 1 | I2C_MASTER_WRITE, ACK_CHECK_EN));
-	ESP_ERROR_CHECK(i2c_master_write(cmd, data, nbytes, ACK_CHECK_EN));
+	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, slave_addr << 1 | I2C_MASTER_WRITE, ACK_EN));
+	ESP_ERROR_CHECK(i2c_master_write(cmd, data, nbytes, ACK_EN));
 	ESP_ERROR_CHECK(i2c_master_stop(cmd));
 
 	i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS);
@@ -45,28 +97,29 @@ static void esp8266_i2c_write(uint32_t const slave_addr, uint8_t *data, uint32_t
 }
 
 /**
-@fn void ssdI2CHardwareInit(void)
-@detail SSD1306 Hardware init configuration for I2C communication.
-
-@warning This function is hardware dependent and must be configured by the user.
-@return void
+* @fn void esp8266_i2c_init(void)
+* @brief I2C hardware interface for ESP8266X
+* @details 
+* I2C hardware initialization. 
+* Using GPIO pin 4 as SDA and GPIO pin 5 as SCL
+* Internal pull-up resistors are not required since the SSD1306 board has soldered pull-up resistors (always check the board)
+* @warning This function is hardware dependent and must be created for the target microcontroller, SoC, etc. 
+* @return void
 */
-void ssdI2CHardwareInit(void){
-	//Board_I2C_Init(I2C0);
-	//Chip_I2C_SetClockRate(I2C0, 1000000);
-	//Chip_I2C_SetMasterEventHandler(I2C0, Chip_I2C_EventHandlerPolling);
 
+void esp8266_i2c_init(void){
 	int i2c_master_port = I2C_NUM_0;
-    i2c_config_t conf;
+	i2c_config_t conf;
 
-    conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = I2C_EXAMPLE_MASTER_SDA_IO;
-    conf.sda_pullup_en = 1;
-    conf.scl_io_num = I2C_EXAMPLE_MASTER_SCL_IO;
-    conf.scl_pullup_en = 1;
-    conf.clk_stretch_tick = 3000; // 300 ticks, Clock stretch is about 210us, you can make changes according to the actual situation.
-	
-    ESP_ERROR_CHECK(i2c_driver_install(i2c_master_port, conf.mode));
-    ESP_ERROR_CHECK(i2c_param_config(i2c_master_port, &conf));
-    //return ESP_OK;
+	conf.mode = I2C_MODE_MASTER;
+	conf.sda_io_num = I2C_SDA_IO;
+	conf.sda_pullup_en = 0;
+	conf.scl_io_num = I2C_SCL_IO;
+	conf.scl_pullup_en = 0;
+	conf.clk_stretch_tick = 300;
+
+	ESP_ERROR_CHECK(i2c_driver_install(i2c_master_port, conf.mode));
+	ESP_ERROR_CHECK(i2c_param_config(i2c_master_port, &conf));
 }
+
+#endif
